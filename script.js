97c166d52c5e8d7a17f4a6fac39ef77bea550afb
Ryder James <stidwell@student.neumont.edu>
2021-02-11 19:52:19 -0700

    Clean and document backend code

--- script.js
+++ script.js
@@ -1,91 +1,121 @@
-const http = require('http');
-const fs = require('fs');
-const { Chess } = require('./chess');
+/** The list of cells yet to be inhabited */
+const availableCells = [0, 1, 2, 3, 4, 5, 6, 7];
 
-let min = (a, b) => {
-	return a < b ? a : b;
-}
-
-let max = (a, b) => {
-	return a > b ? a : b;
-}
-
-let getRandomInRange = (min, max) => {
-	return getRandomInt(max - min + 1) + min;
-}
-
-let getRandomInt = max => {
-	return Math.floor(Math.random() * Math.floor(max));
-}
-
-let availableCells = [0, 1, 2, 3, 4, 5, 6, 7];
-let pieces = {
+/** All of the pieces and their FEN identification character */
+const pieces = {
 	rookA: {index: 0, id: 'r'},	
 	rookB: {index: 0, id: 'r'},	
 	knightA: {index: 0, id: 'n'},	
 	knightB: {index: 0, id: 'n'},	
-	bishopA: {index: 0, id: 'b'},	
+	bishopA: {index: 0, id: 'b'},
 	bishopB: {index: 0, id: 'b'},
 	king: {index: 0, id: 'k'},	
 	queen: {index: 0, id: 'q'},	
 }
 
-let placeRooks = () => {
-	pieces.rookA.index = getRandomInt(8);
-	availableCells.splice(pieces.rookA.index, 1);
+/**
+ * Returns a random integer in the range min-max
+ * @param {Number} min the bottom of the range, inclusive
+ * @param {Number} max the top of the range, inclusive
+ */
+const getRandomInRange = (min, max) => getRandomInt(max - min + 1) + min;
 
-	let rookBIndex = getRandomInt(availableCells.length);
-	pieces.rookB.index = availableCells[rookBIndex];
-	while (Math.abs(pieces.rookA.index - pieces.rookB.index) < 2) {
-		rookBIndex = availableCells[getRandomInt(availableCells.length)];
-		pieces.rookB.index = availableCells[rookBIndex];
-	}
-	availableCells.splice(rookBIndex, 1);
+/**
+ * Returns a number between 0 (inclusive) and max (exclusive)
+ * @param {Number} max the top of the range, exclusive
+ */
+const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+
+/**
+ * Removes the cell matching the piece's index from the list of available cells
+ * @param {Piece} piece 
+ */
+const claimCell = piece => availableCells.splice(piece.index, 1);
+
+/**
+ * Returns true if the piece is on a white cell
+ * @param {Piece} piece 
+ */
+const isOnWhite = piece => piece.index % 2 == 0;
+
+/**
+ * Places the piece in a random available cell. Note that this does not remove
+ * a cell from the list of available cells.
+ * @param {Piece} piece 
+ * @see placeAndClaimRandomCell
+ */
+const placeInRandomCell = piece => piece.index = availableCells[getRandomInt(availableCells.length)];
+
+/**
+ * Places the piece in a random available cell and removes it from the list of
+ * available cells.
+ * @param {Piece} piece 
+ */
+const placeAndClaimRandomCell = piece => {
+	placeInRandomCell(piece);
+	claimCell(piece);
 }
 
-let placeKing = () => {
-	let minI = min(pieces.rookA.index, pieces.rookB.index);
-	let maxI = max(pieces.rookA.index, pieces.rookB.index);
-	pieces.king.index = getRandomInRange(minI + 1, maxI - 1);
+/** 
+ * Places both rooks such that there is at least one space between them
+ */
+const placeRooks = () => {
+	placeAndClaimRandomCell(pieces.rookA);
 
-	availableCells.splice(availableCells.indexOf(pieces.king.index), 1);
+	do {
+		placeInRandomCell(pieces.rookB);
+		// if the rooks do not have a space between them, try again
+	} while (Math.abs(pieces.rookA.index - pieces.rookB.index) < 2);
+	claimCell(pieces.rookB);
 }
 
-let placeBishops = () => {
-	pieces.bishopA.index = availableCells[getRandomInt(availableCells.length)];
-	availableCells.splice(availableCells.indexOf(pieces.bishopA.index), 1);
+/**
+ *  Places the king somewhere between the position of the two rooks 
+ */
+const placeKing = () => {
+	let leftRook = Math.min(pieces.rookA.index, pieces.rookB.index);
+	let rightRook = Math.max(pieces.rookA.index, pieces.rookB.index);
+	pieces.king.index = getRandomInRange(leftRook + 1, rightRook - 1);
+
+	claimCell(pieces.king);
+}
+
+/**
+ * Places the two bishops such that they are on different colors 
+ */
+const placeBishops = () => {
+	placeAndClaimRandomCell(pieces.bishopA);
 	
-	pieces.bishopB.index = availableCells[getRandomInt(availableCells.length)];
-	if (pieces.bishopA.index % 2 == 0 && pieces.bishopB.index % 2 == 0) {
-		do {
-			pieces.bishopB.index = availableCells[getRandomInt(availableCells.length)];
-		} while (availableCells.indexOf(pieces.bishopB.index) == -1);
-	} else if (pieces.bishopB.index % 2 == 1) {
-		do {
-			pieces.bishopB.index = availableCells[getRandomInt(availableCells.length)]
-		} while (availableCells.indexOf(pieces.bishopB.index) == -1);
-	}
-	availableCells.splice(availableCells.indexOf(pieces.bishopB.index), 1);
+	do {
+		placeInRandomCell(pieces.bishopB);
+		// if both pieces are on the same color, try to place again
+	} while (isOnWhite(pieces.bishopA) == isOnWhite(pieces.bishopB));
+	claimCell(pieces.bishopB);
 }
 
-let placeRemainingPieces = () => {
-	pieces.knightA.index = availableCells[getRandomInt(availableCells.length)];
-	availableCells.splice(availableCells.indexOf(pieces.knightA.index), 1);
-
-	pieces.knightB.index = availableCells[getRandomInt(availableCells.length)];
-	availableCells.splice(availableCells.indexOf(pieces.knightB.index), 1);
-	
-	pieces.queen.index = availableCells[0];
+/**
+ * Places the two knights and the queen on random spaces 
+ */
+const placeRemainingPieces = () => {
+	placeAndClaimRandomCell(pieces.knightA);
+	placeAndClaimRandomCell(pieces.knightB);
+	placeAndClaimRandomCell(pieces.queen);
 }
 
-let generate960Board = () => {
+/** 
+ * Generates a board using the Fischer random chess rules
+ */
+const generate960Board = () => {
 	placeRooks();
 	placeKing();
 	placeBishops();
 	placeRemainingPieces();
 }
 
-let generateFENString = () => {
+/**
+ * Constructs a FEN string based on the pieces object 
+ */
+const generateFENString = () => {
 	let piecesArr = Object.values(pieces);
 	piecesArr.sort((a, b) => a.index - b.index);
 	
@@ -97,20 +127,8 @@ let generateFENString = () => {
 	return fenString;
 }
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200, { 'content-type': 'text/html' });
-	fs.createReadStream('index.html').pipe(res);
-});
-
-server.listen(process.env.PORT || 3000);
-
 generate960Board();
 let fen = generateFENString();
 
+console.log(fen);
 const chess = new Chess(fen);
-while (!chess.game_over()) {
-	const moves = chess.moves();
-	const move = moves[getRandomInt(moves.length)];
-	chess.move(move);
-}
-console.log(chess.pgn());
